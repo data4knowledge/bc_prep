a6acb90c4c34ee0236b8775bb3c539a7a683c990
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-10-21 09:20:40 +0200

    Update route for CT service change

--- utility/ct_service.py
+++ utility/ct_service.py
@@ -7,7 +7,7 @@ class CTService():
     self.__api_url = ServiceEnvironment().get("CT_SERVER_URL")
   
   def term_reference(self, cl, cli):
-    return self.api_get("v1/terms?codelist=%s&term=%s" % (cl, cli))
+    return self.api_get("/v1/codeLists/terms?codelist=%s&term=%s" % (cl, cli))
 
   def api_get(self, url):
     headers =  {"Content-Type":"application/json"}
