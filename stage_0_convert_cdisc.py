a4f61bb434e0ca8395a583d6ebadd2d4bc4a49c9
Kirsten Langendorf <kwl@data4knowledge.dk>
2022-10-22 17:57:10 +0200

    Update for CDISC BC import

--- stage_0_convert_cdisc.py
+++ stage_0_convert_cdisc.py
@@ -1,5 +1,10 @@
+import http
 import yaml
 from utility.utility import *
+import requests
+from utility.service_environment import ServiceEnvironment
+
+CTService =  'http://localhost:8012/'
 
 dict = {
   "Base Laboratory":
@@ -28,11 +33,11 @@ def process_instances():
   for filename in files:
     with open(filename, "r+") as file:
       instance = yaml.load(file, Loader=yaml.FullLoader)
-      bc_file_name = instance['datasetSpecializationId']
+      bc_file_name = instance.get('datasetSpecializationId')
       bc = []
       bc = {
         'name': instance['shortName'],
-        'identifier': instance['datasetSpecializationId'],
+        'identifier': instance.get('datasetSpecializationId'),
         'based_on':'',
         'identified_by':
         {
@@ -44,28 +49,53 @@ def process_instances():
         'has_items': []
       } 
       
-      x = {'name':'coding',
-           'value_set': 
-           [
-             {
-              'cl': "TO BE LOOKED UP",
-              'cli': instance['biomedicalConceptId'] 
-             }
-           ]
-          }
-      bc['identified_by']['data_type'].append(x)
-      if filename.__contains__('_lb_'):
-         bc['based_on'] = 'Base Laboratory'
-      else:
+      if instance.get('biomedicalConceptId') is not None:
+        ct_url = CTService + 'v1/terms?identifier=' + instance['biomedicalConceptId']
+        r = requests.get(ct_url)
+        cls = r.json()
+        if not len(cls):
+          print('Codelist for code '+instance['biomedicalConceptId']+' cannot be found')
+          x = {'name':'coding',
+                'value_set': 
+                [
+                {
+                'cl': 'NA' ,
+                'cli': instance['biomedicalConceptId']
+                }
+                ]
+               }
+          bc['identified_by']['data_type'].append(x)
+        else:   
+         for cl in cls :
+        #Get the SDTM codelist for the TESTCD
+          if instance['biomedicalConceptId'] == 'C164634':
+           print('cl is:'+cl)
+          if cl['parent']['uri'].__contains__('sdtm') & cl['parent']['uri'].__contains__('2022') & cl['parent']['notation'].__contains__('TESTCD') :
+           codeList = cl['parent']['identifier'] 
+                     
+           x = {'name':'coding',
+                'value_set': 
+                [
+                {
+                'cl': codeList ,
+                'cli': instance['biomedicalConceptId']
+                }
+                ]
+               }
+           bc['identified_by']['data_type'].append(x)
+        if filename.__contains__('_lb_'):
+          bc['based_on'] = 'Base Laboratory'
+        else:
           bc['based_on'] = 'Base Observation'   
       
-      bc['has_items'] = []
-      for var in instance['variables']:        
+        bc['has_items'] = []
+        if instance.get('variables') is not None:
+         for var in instance['variables']:        
         #replace the e.g. VSPOS to --POS
-        var['name'] =  '--' +  var['name'][2:]
-        if  var['name'] not in(['--TEST','--TESTCD','--ORRESU','--STRESC','--STRESN','--STRESU','--CAT','--LOINC','--FAST']) : 
-          if 'dataType' in var:
-           record = {
+          var['name'] =  '--' +  var['name'][2:]
+          if var['name'] not in(['--TEST','--TESTCD','--ORRESU','--STRESC','--STRESN','--STRESU','--CAT','--LOINC','--FAST']) : 
+           if 'dataType' in var:
+            record = {
             'name': dict[bc['based_on']][var['name']],
             'enabled':True,
             'data_type': 
@@ -75,7 +105,7 @@ def process_instances():
               }
             ]
           }
-          else:
+           else:
             record = {
             'name': dict[bc['based_on']][var['name']],
             'enabled':True,
@@ -87,20 +117,36 @@ def process_instances():
               }
             ]
           }
-            bc['has_items'].append(record)
+           bc['has_items'].append(record)
             
           if 'valueList' in var:
             for value in var['valueList']:
-              rec = {
-                      'cl':"TO BE LOOKED UP",
-                      "cli": value
-                    }
-              record['data_type'][0]['value_set'].append(rec)
+              if value.__contains__('NEW'):
+               rec = {
+                     'cl': 'NA',
+                     "cli": value
+                   }
+               record['data_type'][0]['value_set'].append(rec)
+              else:
+               cli_url = CTService + 'v1/terms?notation=' + value
+               x = requests.get(cli_url)
+               cli = x.json()
+               for cl in cli :
+                if cl['parent']['uri'].__contains__('sdtm') & cl['parent']['uri'].__contains__('2022') :
+                 if cl['parent']['notation'] != 'PKUNIT' : 
+                  codeList = cl['parent']['identifier']
+                  codeListItem = cl['child']['identifier']
+                  rec = {
+                     'cl': codeList,
+                     "cli": codeListItem
+                   }
+                  record['data_type'][0]['value_set'].append(rec)
             
-    if filename.find('_lb_'):
+    #if filename.__contains__('_vs_'):
       path = 'converted_data/'
-      filename = path+bc_file_name+'.yaml'
-      with open(filename, 'w') as file:
+      if bc_file_name is not None:
+       filename = path+bc_file_name+'.yaml'
+       with open(filename, 'w') as file:
         yaml.dump([bc], file,sort_keys=False)
 
 process_instances()                
---
+++ stage_1_build_cdisc.py
@@ -0,0 +1,228 @@
+import yaml
+from utility.utility import *
+from utility.ra_service import *
+from utility.crm_service import *
+from utility.ct_service import *
+from stringcase import pascalcase, snakecase
+from utility.fhir import add_data_type
+
+nodes = { 
+  "Template": [], "Instance": [], "TemplateItem": [], "InstanceItem": [], "DataType": [], "DataTypeProperty": [], "ValueSet": [],
+  'ScopedIdentifier': [], 'Namespace': [], 'RegistrationStatus': [], 'RegistrationAuthority': [] 
+}
+relationships = { 
+  "BASED_ON": [], "HAS_ITEM": [], "HAS_IDENTIFIER": [], "HAS_QUALIFIER": [], "BC_NARROWER": [], "HAS_DATA_TYPE": [], "HAS_PROPERTY": [], "HAS_RESPONSE": [],
+  "IDENTIFIED_BY": [], "HAS_STATUS": [], "SCOPED_BY": [], "MANAGED_BY": [],
+}
+bc_uri = {}
+crm_paths = {}
+
+def process_templates(the_instance_uri, ns_uri, ra_uri):
+  with open("source_data/templates/templates.yaml") as file:
+    templates = yaml.load(file, Loader=yaml.FullLoader)
+    for template in templates:
+      the_template_uri = template_uri(the_instance_uri, template["name"])
+      #print("Template:", template["name"], the_template_uri)
+      nodes["Template"].append({"name": template["name"], "uri": the_template_uri, "uuid": uuid4() })
+      add_identifier_and_status(the_template_uri, template["name"].upper(), "2022-09-01", ns_uri, ra_uri, nodes, relationships)
+      name = format_name(template["identified_by"]["name"])
+      item_uri = "%s/%s" % (the_template_uri, name)
+      record = {
+        "name": template["identified_by"]["name"], 
+        "mandatory": template["identified_by"]["mandatory"],
+        "enabled": template["identified_by"]["enabled"],
+        "uri": item_uri,
+        "uuid": uuid4(),
+        "canonical": ""
+      }
+      if "canonical" in template["identified_by"]:
+        record["canonical"] = template["identified_by"]["canonical"]
+        #crm_server = CRMServer()
+        #result = crm_server.crm_node_data_types(template["identified_by"]["canonical"])
+        #crm_paths[template["identified_by"]["canonical"]] = result
+      nodes["TemplateItem"].append(record)
+      relationships["HAS_ITEM"].append({"from": the_template_uri, "to": item_uri})
+      relationships["HAS_IDENTIFIER"].append({"from": the_template_uri, "to": item_uri})
+
+      parent_uri = item_uri
+      name = format_name(template["identified_by"]["data_type"][0]["name"])
+      item_uri = "%s/%s" % (parent_uri, name)
+      record = {
+        "name": template["identified_by"]["data_type"][0]["name"],
+        "uri": item_uri,
+        "uuid": uuid4()
+      }
+      nodes["DataType"].append(record)
+      relationships["HAS_DATA_TYPE"].append({"from": parent_uri, "to": item_uri})
+
+      # Now all the items
+      for item in template["has_items"]: 
+        name = format_name(item["name"])
+        item_uri = "%s/%s" % (the_instance_uri, name)
+        record = {
+          "name": item["name"], 
+          "mandatory": item["mandatory"],
+          "enabled": item["enabled"],
+          "canonical": "",
+          "uri": item_uri,
+          "uuid": uuid4()
+        }
+        if "canonical" in item:
+          record["canonical"] = item["canonical"]
+          #crm_server = CRMServer()
+          #result = crm_server.crm_node_data_types(item["canonical"])
+          #print(result)
+          #crm_paths[template["identified_by"]["canonical"]] = result
+        nodes["TemplateItem"].append(record)
+        relationships["HAS_ITEM"].append({"from": the_template_uri, "to": item_uri})
+        parent_uri = item_uri
+        for data_type in item["data_type"]: 
+          name = format_name(data_type["name"])
+          item_uri = "%s/%s" % (parent_uri, name)
+          record = {
+            "name": data_type["name"],
+            "uri": item_uri,
+            "uuid": uuid4()
+          }
+          nodes["DataType"].append(record)
+          relationships["HAS_DATA_TYPE"].append({"from": parent_uri, "to": item_uri})
+
+def process_instances(base_uri, ns_uri, ra_uri):
+  ct_server = CTService()
+  files = files_in_dir('converted_data')
+  for filename in files:
+    with open(filename) as file:
+      narrower = {}
+      instances = yaml.load(file, Loader=yaml.FullLoader)
+      for instance in instances:
+        #print(instance)
+        #print("instance:", instance["name"])
+        based_on_uri = template_uri(base_uri, instance["based_on"])
+        #print("based on:", based_on_uri)
+        the_instance_uri, uri_name = instance_uri(base_uri, instance["name"])
+        nodes["Instance"].append({ "name": instance["name"], "uri": the_instance_uri, "uuid": uuid4() })           
+        relationships["BASED_ON"].append({"from": the_instance_uri, "to": based_on_uri})
+        add_identifier_and_status(the_instance_uri, instance["name"].upper(), "2022-09-01", ns_uri, ra_uri, nodes, relationships)
+        bc_uri[uri_name] = the_instance_uri
+        narrower[the_instance_uri] = []
+        if "narrower" in instance:
+          for children in instance["narrower"]:
+            narrower[the_instance_uri].append(format_name(children))
+        # Identifier Node and Associated Data Type
+        item = instance["identified_by"]
+        qualifier_item = ""
+        if "qualified_by" in item:
+          qualifier_item = item["qualified_by"]
+        name = format_name(item["name"])
+        item_uri = "%s/%s" % (the_instance_uri, name)
+        identifier_uri = item_uri
+        collect = False
+        if "collect" in item:
+          collect = item["collect"]
+        record = {
+          "name": item["name"], 
+          "collect": collect,
+          "enabled": item["enabled"],
+          "uri": item_uri,
+          "uuid": uuid4()
+        }
+        nodes["InstanceItem"].append(record)
+        relationships["HAS_ITEM"].append({"from": the_instance_uri, "to": item_uri})
+        relationships["HAS_IDENTIFIER"].append({"from": the_instance_uri, "to": item_uri})
+        if "data_type" in item:
+          for data_type in item["data_type"]: 
+            dt_uri = add_data_type(item_uri, data_type["name"], nodes, relationships)
+            relationships["HAS_DATA_TYPE"].append({"from": item_uri, "to": dt_uri})
+            if "value_set" in data_type:
+              #print(data_type["value_set"])
+              for term in data_type["value_set"]: 
+                #print(term)
+                cl = term["cl"]
+                cli = term["cli"]
+                result = ct_server.term_reference(cl, cli)
+                print(result[2]['parent']['uri'])
+                print(result[2]['child']['uri'])
+                if result == None:
+                  result = { 'uri': "", 'notation': "", 'pref_label': "" }
+                record = {
+                  "uuid": str(uuid4()),
+                  "cl": cl,
+                  "cli": cli,
+                  "term_uri": result['child']['uri'],
+                  "notation": result['child']['notation'],
+                  "pref_label": result['child']['pref_label']
+                }
+                nodes["ValueSet"].append(record)
+                relationships["HAS_RESPONSE"].append({"from": dt_uri, "to": record['uuid']})
+
+        # Now all the items
+        for item in instance["has_items"]: 
+          #print("Enabled:", item["enabled"])
+          if not item["enabled"]:
+            continue
+          name = format_name(item["name"])
+          collect = False
+          if "collect" in item:
+            collect = item["collect"]
+          item_uri = "%s/%s" % (the_instance_uri, name)
+          record = {
+            "name": item["name"], 
+            "collect": collect,
+            "enabled": item["enabled"],
+            "uri": item_uri,
+            "uuid": uuid4()
+          }
+          nodes["InstanceItem"].append(record)
+          relationships["HAS_ITEM"].append({"from": the_instance_uri, "to": item_uri})
+          #print("Rel: [from: %s, to: %s]" % (the_instance_uri, item_uri))
+          if qualifier_item == item["name"]:
+            relationships["HAS_QUALIFIER"].append({"from": identifier_uri, "to": item_uri})
+          if "data_type" in item:
+            for data_type in item["data_type"]: 
+              dt_uri = add_data_type(item_uri, data_type["name"], nodes, relationships)
+              relationships["HAS_DATA_TYPE"].append({"from": item_uri, "to": dt_uri})
+              if "value_set" in data_type:
+                #print(data_type["value_set"])
+                for term in data_type["value_set"]: 
+                  #print(term)
+                  cl = term["cl"]
+                  cli = term["cli"]
+                  result = ct_server.term_reference(cl, cli)
+                  if result == None:
+                    result = { 'uri': "", 'notation': "", 'pref_label': "" }
+                  record = {
+                    "uuid": str(uuid4()),
+                    "cl": cl,
+                    "cli": cli,
+                    "term_uri": result['uri'],
+                    "notation": result['notation'],
+                    "pref_label": result['pref_label']
+                  }
+                  nodes["ValueSet"].append(record)
+                  relationships["HAS_RESPONSE"].append({"from": dt_uri, "to": record['uuid']})
+
+    for k, v in narrower.items():
+      if len(v) > 0:
+        from_uri = k
+        for bc in v:
+          to_uri = bc_uri[bc]
+          #print("Narrower from %s to %s" % (from_uri, to_uri))
+          relationships["BC_NARROWER"].append({"from": from_uri, "to": to_uri})
+
+delete_dir("load_cdisc_data")
+
+ns_s_json = RaService().namespace_by_name("CDISC BC namespace")
+#print(ns_s_json)
+ra_s_json = RaService().registration_authority_by_namespace_uuid(ns_s_json['uuid'])
+#print(ra_s_json)
+
+process_templates(ns_s_json['value'], ns_s_json['uri'], ra_s_json['uri'])
+process_instances(ns_s_json['value'], ns_s_json['uri'], ra_s_json['uri'])
+
+for k, v in nodes.items():
+  csv_filename = "load_cdisc_data/node-%s-1.csv" % (snakecase(k))
+  write_nodes(v, csv_filename)
+
+for k, v in relationships.items():
+  csv_filename = "load_cdisc_data/relationship-%s-1.csv" % (k.lower())
+  write_relationships(v, csv_filename)
